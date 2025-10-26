f5ab03601012843ad1f8abce6b043f5fde3c3ee8
kanhaiya-18 <nayakkanhaiya1@gmail.com>
2025-10-26 12:38:44 +0530

    added equal split functionality

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const upload = require("../middleware/upload");
-const { uploadBill, parseBill, assignMoney, recordPayment, splitExpense, getBillDetails, settleAssignments, assignEqually } = require("../controllers/billController");
-const { parse } = require("path");
+const { uploadBill,  assignMoney, recordPayment, splitExpense, getBillDetails, settleAssignments, assignEqually } = require("../controllers/billController");
+// const { parse } = require("path");
 
 router.post("/upload", auth,upload.single("bill"),uploadBill);
 // router.post("/parse",auth,parseBill);
